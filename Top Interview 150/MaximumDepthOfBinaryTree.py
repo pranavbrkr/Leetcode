6c96271c57231e4f2fa0360d6bc4499003d135bf
pranavbrkr <pranavbrkr@gmail.com>
2023-05-29 17:19:17 +0530

    Max Binary Tree Depth

---
+++ Top Interview 150/MaximumDepthOfBinaryTree.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
