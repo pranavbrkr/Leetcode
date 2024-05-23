5fe836303cb6bbb98c884d20d686e28afe38d625
pranavbrkr <pranavbrkr@gmail.com>
2024-05-23 12:53:59 -0700

    top k freq elements

---
+++ TopKFrequentElements.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        num_count = dict()
+
+        for number in nums:
+            if number in num_count.keys():
+                num_count[number] += 1
+            else:
+                num_count[number] = 1
+
+        num_count_sorted = sorted(num_count.items(), key=lambda x: x[1], reverse = True)
+
+        answer = []
+        for i in range(k):
+            answer.append(num_count_sorted[i][0])
+
+        return answer
