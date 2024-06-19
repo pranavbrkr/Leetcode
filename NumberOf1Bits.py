b31018fbca8177477515fe81f3224ea5ff738fbf
pranavbrkr <pranavbrkr@gmail.com>
2024-06-18 18:45:00 -0700

    num of 1 bits and climbing stairs

--- NumberOf1Bits.py
+++ NumberOf1Bits.py
@@ -7,4 +7,10 @@ class Solution(object):
         :rtype: int
         """
 
-        return bin(n).count("1")
+        answer = 0
+        while n != 0:
+            if n % 2 != 0:
+                answer += 1
+            n = n >> 1
+
+        return answer
