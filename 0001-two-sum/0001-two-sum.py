866e2dfdfbf289c340c85394616900b924e1e788
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-02-21 03:55:14 +0530

    Time: 0 ms (100%), Space: 13.3 MB (31.76%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mydict = dict()
+        nums_len = len(nums)
+        ret_list = []
+
+        for i in range(nums_len):
+            mydict[nums[i]] = i
+        
+        for i in range(nums_len):
+            pair_index = mydict.get(target - nums[i])
+            if pair_index is not None and pair_index != i:
+                ret_list.append(i)
+                ret_list.append(pair_index)
+                break
+
+        return ret_list
