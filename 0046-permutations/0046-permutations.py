eadc223aa522a629bef563fc59aea5a1c7e61da3
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-02-23 01:06:11 +0530

    Time: 0 ms (100%), Space: 17.7 MB (94.88%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,12 +3,13 @@ class Solution:
         if len(nums) == 0:
             return [[]]
 
-        perms = self.permute(nums[1:])
+        permutations = self.permute(nums[1:])
         answer = []
-        for p in perms:
-            for i in range(len(p) + 1):
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                answer.append(p_copy)
+
+        for perm in permutations:
+            for i in range(len(perm) + 1):
+                perm_copy = perm.copy()
+                perm_copy.insert(i, nums[0])
+                answer.append(perm_copy)
         
         return answer
