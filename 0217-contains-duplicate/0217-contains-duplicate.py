f3f5bccba5b1fe350dad1be5c380586e4ac840e3
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-03-13 18:32:29 -0700

    Time: 15 ms (28.15%), Space: 31.6 MB (30.1%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arr_set = set()
+
+        for number in nums:
+            if number in arr_set:
+                return True
+            arr_set.add(number)
+        
+        return False
