61a53d4892b2849ab61e28feca2769aa02e3dc9e
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-04-26 11:38:52 -0700

    Time: 7 ms (80.66%), Space: 18.2 MB (48.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,13 +5,14 @@ class Solution:
         while l < r:
             while l < r and not s[l].isalnum():
                 l += 1
-            
+
             while l < r and not s[r].isalnum():
                 r -= 1
             
             if s[l].lower() != s[r].lower():
                 return False
             
-            l, r = l + 1, r - 1
-        
+            l += 1
+            r -= 1
+
         return True
