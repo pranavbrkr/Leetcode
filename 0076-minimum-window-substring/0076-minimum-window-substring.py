e50a429cd0e97323396c273955bbd3d1e71bfb59
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-04-28 13:40:11 -0700

    Time: 63 ms (81.75%), Space: 18.1 MB (93.11%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,21 +2,22 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "":
             return ""
-        
-        count_t, window = defaultdict(int), defaultdict(int)
 
-        for c in t:
-            count_t[c] += 1
+        countt, window = defaultdict(int), defaultdict(int)
 
-        have, need = 0, len(count_t)
-        answer, answer_len = [-1, -1], float("inf")
+        for char in t:
+            countt[char] += 1
+
+        answer = [-1, -1]
+        answer_len = float('inf')
+        have, need = 0, len(countt)
+
         l = 0
-
         for r in range(len(s)):
             c = s[r]
             window[c] += 1
 
-            if c in count_t and window[c] == count_t[c]:
+            if c in countt and window[c] == countt[c]:
                 have += 1
             
             while have == need:
@@ -25,9 +26,10 @@ class Solution:
                     answer_len = r - l + 1
                 
                 window[s[l]] -= 1
-                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
+                if s[l] in countt and window[s[l]] < countt[s[l]]:
                     have -= 1
                 l += 1
-            
+        
         l, r = answer
-        return s[l : r + 1] if answer != float("inf") else ""
+        
+        return s[l:r + 1] if answer_len != float("inf") else ""
