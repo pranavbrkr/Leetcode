23441e102e7b705534fd3582ab568ad35edd0b16
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-04-29 18:38:12 -0700

    Time: 150 ms (45.61%), Space: 78.7 MB (27.31%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+    def __init__(self, key, value):
+        self.key, self.val = key, value
+        self.next = self.prev = None
 
 class LRUCache:
 
@@ -9,17 +9,17 @@ class LRUCache:
         self.capacity = capacity
         self.cache = {}
 
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-    
+        self.right, self.left = Node(0, 0), Node(0, 0)
+        self.right.prev, self.left.next = self.left, self.right
+        
     def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
 
     def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        prv, nxt = self.right.prev, self.right
+        prv.next = nxt.prev = node
+        node.prev, node.next = prv, nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
