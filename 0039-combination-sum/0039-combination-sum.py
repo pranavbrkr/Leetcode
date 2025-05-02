a7d92c6b36650c08b2c9cb6d14b2f4e422f2fb8b
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-02 10:29:49 -0700

    Time: 13 ms (35.2%), Space: 18.1 MB (20.05%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,19 +3,19 @@ class Solution:
         answer = []
 
         def dfs(i, curr, total):
-            if  total == target:
+            if total == target:
                 answer.append(curr.copy())
                 return
             
             if i >= len(candidates) or total > target:
                 return
-            
+
             curr.append(candidates[i])
             dfs(i, curr, total + candidates[i])
 
             curr.pop()
             dfs(i + 1, curr, total)
-            
+
             return
         
         dfs(0, [], 0)
