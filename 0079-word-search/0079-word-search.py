a7e264541b36d986f9f456fd5109219a29e4da40
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-02 11:12:16 -0700

    Time: 4283 ms (55.48%), Space: 17.9 MB (64.84%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        word_len = len(word)
         m, n = len(board), len(board[0])
         path = set()
-        word_len = len(word)
 
-        def dfs(row, col, index):
+        def dfs(index, row, col):
             if index >= word_len:
                 return True
             
-            if (row >= m or
-               col >= n or 
-               row < 0 or
-               col < 0 or 
-               board[row][col] != word[index] or 
-               (row, col) in path):
+            if row >= m or col >= n or row < 0 or col < 0 or board[row][col] != word[index] or (row, col) in path:
                 return False
             
             path.add((row, col))
-
-            dfs_result = (dfs(row + 1, col, index + 1) or
-                         dfs(row - 1, col, index + 1) or
-                         dfs(row, col + 1, index + 1) or
-                         dfs(row, col - 1, index + 1))
+            dfs_result = (dfs(index + 1, row + 1, col) or
+                         dfs(index + 1, row - 1, col) or
+                         dfs(index + 1, row, col + 1) or
+                         dfs(index + 1, row, col - 1))
             
             path.remove((row, col))
-            
             return dfs_result
         
         for i in range(m):
             for j in range(n):
-                if dfs(i, j, 0):
+                if dfs(0, i, j):
                     return True
         
         return False
