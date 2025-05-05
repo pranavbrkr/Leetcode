828d757756bbb7c00bb3d650ad85d196fd03aade
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-05 13:25:22 -0700

    Time: 6 ms (65.12%), Space: 18.5 MB (63.46%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,33 +1,33 @@
 class Solution:
     def getAdjacentMines(self, board, x, y):
         num_mines = 0
-        for r in range(x - 1, x + 2):
-            for c in range(y - 1, y + 2):
-                if (r >= 0 and r < len(board) and
-                    c >= 0 and c < len(board[0]) and
-                    board[r][c] == 'M'):
-                    num_mines += 1
+        m = len(board)
+        n = len(board[0])
 
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if (i >= 0 and i < m) and (j >= 0 and j < n) and board[i][j] == 'M':
+                    num_mines += 1
         return num_mines
 
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         if not board:
             return board
-        
+
+        m = len(board)
+        n = len(board[0])
+
         x, y = click
         if board[x][y] == 'M':
             board[x][y] = 'X'
         else:
-            num_mines = self.getAdjacentMines(board, x, y)
-            if num_mines:
-                board[x][y] = str(num_mines)
+            mines = self.getAdjacentMines(board, x, y)
+            if mines:
+                board[x][y] = str(mines)
             else:
                 board[x][y] = 'B'
-                for r in range(x - 1, x + 2):
-                    for c in range(y - 1, y + 2):
-                        if (r >= 0 and r < len(board) and
-                            c >= 0 and c < len(board[0]) and
-                            board[r][c] != 'B'):
-                            self.updateBoard(board, [r, c])
-
+                for i in range(x - 1, x + 2):
+                    for j in range(y - 1, y + 2):
+                        if (i >= 0 and i < m) and (j >= 0 and j < n) and board[i][j] != 'B':
+                            self.updateBoard(board, [i, j])
         return board
