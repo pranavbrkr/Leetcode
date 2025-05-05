e8f16dee18f54b669746b97c1e7b1b9ab554cb5c
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-05 12:22:16 -0700

    Time: 8 ms (25.39%), Space: 19.6 MB (24.37%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -4,25 +4,24 @@ class Solution:
             return 1
         
         num_components = 0
-
-        adj = {node: [] for node in range(n)}
+        adj = {i: [] for i in range(n)}
 
         for n1, n2 in edges:
             adj[n1].append(n2)
             adj[n2].append(n1)
-        
+
         visited = set()
 
         def dfs(node):
-            for neighbor in adj[node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    dfs(neighbor)
+            for nei in adj[node]:
+                if nei not in visited:
+                    visited.add(nei)
+                    dfs(nei)
         
-        for node in adj:
-            if node not in visited:
-                visited.add(node)
+        for i in adj:
+            if i not in visited:
+                visited.add(i)
                 num_components += 1
-                dfs(node)
+                dfs(i)
         
         return num_components
