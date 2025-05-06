ba43f416b911540075345fcf42e49d1c36d451e3
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-06 15:07:01 -0700

    Time: 0 ms (100%), Space: 17.7 MB (85.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,12 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
         def helper(arr):
-            rob1 = 0
-            rob2 = 0
-
-            for i in range(len(arr)):
-                temp = max(rob1 + arr[i], rob2)
+            rob1, rob2 = 0, 0
+            for house in arr:
+                temp = max(rob1 + house, rob2)
                 rob1 = rob2
                 rob2 = temp
             
