aaf721ebc772061dde298f5cdd0199daa611e6d5
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-19 22:16:55 -0700

    Time: 4 ms (14.16%), Space: 17.7 MB (95.22%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -2,13 +2,15 @@ class Solution:
     def alienOrder(self, words: List[str]) -> str:
         adj = {c: set() for w in words for c in w}
         indegree = {c: 0 for c in adj}
-        
+
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            minLen = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+            w1 = words[i]
+            w2 = words[i + 1]
+
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
                 return ""
-            for j in range(minLen):
+            for j in range(min_len):
                 if w1[j] != w2[j]:
                     if w2[j] not in adj[w1[j]]:
                         adj[w1[j]].add(w2[j])
@@ -16,17 +18,19 @@ class Solution:
                     break
         
         q = deque([c for c in indegree if indegree[c] == 0])
-        res = []
-        
+        answer = []
+        print(indegree)
+
         while q:
-            char = q.popleft()
-            res.append(char)
-            for neighbor in adj[char]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
+            c = q.popleft()
+            print(c)
+            answer.append(c)
+            for nei in adj[c]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
         
-        if len(res) != len(indegree):
+        if len(answer) != len(indegree):
             return ""
         
-        return "".join(res)
+        return "".join(answer)
