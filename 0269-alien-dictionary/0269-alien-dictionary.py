908f4276c4368d4a0e4fdba337e57173a0d02db8
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-05-19 21:52:48 -0700

    Time: 0 ms (100%), Space: 17.8 MB (79.37%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -3,11 +3,13 @@ class Solution:
         adj = {c: set() for w in words for c in w}
 
         for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            minLen = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+            w1 = words[i]
+            w2 = words[i + 1]
+
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
                 return ""
-            for j in range(minLen):
+            for j in range(min_len):
                 if w1[j] != w2[j]:
                     adj[w1[j]].add(w2[j])
                     break
@@ -18,19 +20,18 @@ class Solution:
         def dfs(c):
             if c in visited:
                 return visited[c]
-            
-            visited[c] = True
 
+            visited[c] = True
             for nei in adj[c]:
                 if dfs(nei):
                     return True
-
+            
             visited[c] = False
             answer.append(c)
         
         for c in adj:
             if dfs(c):
                 return ""
-        
+
         answer.reverse()
         return "".join(answer)
