9c0289aaf0ce68d04435b63c518cb2eba76380ce
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-02 10:31:26 -0700

    Time: 2745 ms (19.25%), Space: 101.2 MB (41.93%) - LeetHub

---
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+
+        edges = []
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                edges.append([abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]), i, j])
+        
+        edges.sort()
+        
+        min_cost = 0
+        parent = [i for i in range(n)]
+        rank = [1 for i in range(n)]
+
+        def find(node):
+            res = node
+            while res != parent[res]:
+                parent[res] = parent[parent[res]]
+                res = parent[res]
+            return res
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            
+            if rank[p1] < rank[p2]:
+                p1, p2 = p2, p1
+            rank[p1] += rank[p2]
+            parent[p2] = p1            
+            return True
+
+        for cost, i, j in edges:
+            if union(i, j):
+                print(i, j)
+                min_cost += cost
+        
+        return min_cost
