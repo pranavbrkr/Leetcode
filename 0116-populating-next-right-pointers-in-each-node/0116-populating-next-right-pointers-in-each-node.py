d91ad7db58f1faf93908f9e6c255de45cb99b901
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-03 16:19:44 -0700

    Time: 55 ms (32.04%), Space: 19.2 MB (51.24%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,20 +10,21 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        depth_map = {}
+        if not root:
+            return None
 
-        def dfs(node, depth):
-            if not node:
-                return
-            
-            if depth not in depth_map:
-                depth_map[depth] = node
-            else:
-                depth_map[depth].next = node
-                depth_map[depth] = node
+        q = deque([root])
 
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
-
-        dfs(root, 0)
+        while q:
+            levelsize = len(q)
+            while levelsize:
+                node = q.popleft()
+                if levelsize > 1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                levelsize -= 1
+        
         return root
