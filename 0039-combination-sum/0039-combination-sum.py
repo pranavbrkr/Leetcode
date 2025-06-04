bf18cf4d4b23d6cbcb26762adfb8e051a9878bae
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-04 10:25:14 -0700

    Time: 4 ms (90.22%), Space: 18 MB (19.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         answer = []
-
-        def dfs(i, curr, total):
+        def backtrack(i, path, total):
             if total == target:
-                answer.append(curr.copy())
+                answer.append(path.copy())
                 return
-            
-            if i >= len(candidates) or total > target:
+            if total>target:
                 return
+            for j in range(i, len(candidates)):
+                path.append(candidates[j])
+                backtrack(j, path, total + candidates[j])
+                path.pop()
 
-            curr.append(candidates[i])
-            dfs(i, curr, total + candidates[i])
-
-            curr.pop()
-            dfs(i + 1, curr, total)
-
-            return
-        
-        dfs(0, [], 0)
+        backtrack(0, [], 0)
         return answer
