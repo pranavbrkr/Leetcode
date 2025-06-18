9c37a4c9f5fe6282b3c0063041c08e32f15f806e
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-18 14:30:13 -0700

    Time: 1 ms (47.7%), Space: 18.2 MB (15.31%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         answer = []
-        freq = [False] * len(nums)
 
-        def backtrack(curr):
-            if len(curr) == len(nums):
-                answer.append(curr.copy())
+        def backtrack(index):
+            if index == len(nums):
+                answer.append(nums[:])
                 return
             
-            for i in range(len(nums)):
-                if not freq[i]:
-                    freq[i] = True
-                    curr.append(nums[i])
-                    backtrack(curr)
-                    curr.pop()
-                    freq[i] = False
+            for i in range(index, len(nums)):
+                nums[i], nums[index] = nums[index], nums[i]
+                backtrack(index + 1)
+                nums[i], nums[index] = nums[index], nums[i]
             
             return
         
-        backtrack([])
+        backtrack(0)
         return answer
