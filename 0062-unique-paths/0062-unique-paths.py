1fbda2927a7daab0024d53365de98a466c78167f
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-23 12:01:21 -0700

    Time: 0 ms (100%), Space: 17.9 MB (25.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,9 +1,22 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        num_paths = [[1 for _ in range(n)] for _ in range(m)]
-
-        for i in range(1, m):
-            for j in range(1, n):
-                num_paths[i][j] = num_paths[i - 1][j] + num_paths[i][j - 1]
         
-        return num_paths[m - 1][n - 1]
+        cache = [[-1] * n for _ in range(m)]
+        cache[0][0] = 1
+        def recursion(i, j):
+            if i == 0 and j == 0:
+                return cache[i][j]
+            
+            if i < 0 or j < 0:
+                return 0
+
+            if cache[i][j] != -1:
+                return cache[i][j]
+            
+            up = recursion(i - 1, j)
+            down = recursion(i, j - 1)
+
+            cache[i][j] = up + down
+            return cache[i][j]
+        
+        return recursion(m - 1, n - 1)
