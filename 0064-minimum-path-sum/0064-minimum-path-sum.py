2520472ade76a92c04a2c0512c97d5ec39173f9e
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-23 13:37:08 -0700

    Time: 27 ms (27.34%), Space: 20.9 MB (19.53%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,20 +3,19 @@ class Solution:
         m, n = len(grid), len(grid[0])
         cache = [[-1] * n for _ in range(m)]
 
-        def recursion(i, j):
-            if i == 0 and j == 0:
-                return grid[0][0]
-            
-            if i < 0 or j < 0:
-                return float('inf')
-            
-            if cache[i][j] != -1:
-                return cache[i][j]
-
-            up = grid[i][j] + recursion(i - 1, j)
-            left = grid[i][j] + recursion(i, j - 1)
-
-            cache[i][j] = min(up, left)
-            return cache[i][j]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    cache[i][j] = grid[0][0]
+                    continue
+                
+                up = left = float('inf')
+                if i > 0:
+                    up = cache[i - 1][j]
+                if j > 0:
+                    left = cache[i][j - 1]
+                
+                cache[i][j] = grid[i][j] + min(up, left)
         
-        return recursion(len(grid) - 1, len(grid[0]) - 1)
+        print(cache)
+        return cache[m - 1][n - 1]
