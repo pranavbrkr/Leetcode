de31ea433ff5b8fe92d7e0ae1dc792821be4cea9
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-23 10:42:26 -0700

    Time: 0 ms (100%), Space: 17.7 MB (82.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        cache = [-1] * len(nums)
-        cache[0] = nums[0]
+        prev = nums[0]
+        prev2 = 0
 
         for i in range(1, len(nums)):
-            pick = nums[i]
-            if i > 1:
-                pick += cache[i - 2]
-            non_pick = cache[i - 1]
-
-            cache[i] = max(pick, non_pick)
-
-        return cache[-1]
+            temp = max(prev, prev2 + nums[i])
+            prev2 = prev
+            prev = temp
+        
+        return prev
