d3cc0d65b7b4a12c2afd6014b06f59a4fa2c467f
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-23 10:33:20 -0700

    Time: 0 ms (100%), Space: 17.7 MB (54.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
-
-        for house in nums:
-            temp = max(house + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
         
-        return rob2
+        cache = [-1] * len(nums)
+        cache[0] = nums[0]
+
+        def recursion(index):
+            if cache[index] != -1:
+                return cache[index]
+            if index == 0:
+                return cache[index]
+            
+            if index < 0:
+                return 0
+            
+            pick = nums[index] + recursion(index - 2)
+            non_pick = recursion(index - 1)
+
+            cache[index] = max(pick, non_pick)
+            return cache[index]
+
+        return recursion(len(nums) - 1)
