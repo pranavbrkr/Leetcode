18ae134bceea9648e9cce76babe83ffdfa611a98
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-28 16:26:04 -0700

    Time: 215 ms (60.02%), Space: 38.9 MB (50.07%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,15 +1,24 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
-        for i in range(len(coins) - 1, -1, -1):
-            next_dp = [0] * (amount + 1)
-            next_dp[0] = 1
 
-            for a in range(1, amount + 1):
-                next_dp[a] = dp[a]
-                if a - coins[i] >= 0:
-                    next_dp[a] += next_dp[a - coins[i]]
-            dp = next_dp
+        dp = [[-1] * (amount + 1) for _ in range(len(coins))]
         
-        return dp[amount]
+        def recursion(index, target):
+            if index == 0:
+                if target % coins[0] == 0:
+                    return 1
+                else:
+                    return 0
+            
+            if dp[index][target] != -1:
+                return dp[index][target]
+            
+            not_take = recursion(index - 1, target)
+            take = 0
+            if coins[index] <= target:
+                take = recursion(index, target - coins[index])
+            
+            dp[index][target] = take + not_take
+            return dp[index][target]
+        
+        return recursion(len(coins) - 1, amount)
