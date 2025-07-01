a81ff4fab208904b000854f92c477dca1c0e8c3a
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-06-30 17:33:55 -0700

    Time: 1203 ms (43.08%), Space: 102.9 MB (19.72%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -3,19 +3,38 @@ class Solution:
         n = len(s)
         s_rev = s[::-1]
 
-        dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+        dp = [[-1 for _ in range(n)] for _ in range(n)]
 
-        for i in range(n + 1):
-            dp[0][i] = 0
-            dp[i][0] = 0
+        def recursion(i, j):
+            if i < 0 or j < 0:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            if s[i] == s_rev[j]:
+                dp[i][j] = 1 + recursion(i - 1, j - 1)
+            else:
+                dp[i][j] = max(recursion(i - 1, j), recursion(i, j - 1))
+            
+            return dp[i][j]
+        
+        return recursion(n - 1, n - 1)
+
+
+        # dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+        # for i in range(n + 1):
+        #     dp[0][i] = 0
+        #     dp[i][0] = 0
 
         
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if s[i - 1] == s_rev[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                    continue
+        # for i in range(1, n + 1):
+        #     for j in range(1, n + 1):
+        #         if s[i - 1] == s_rev[j - 1]:
+        #             dp[i][j] = dp[i - 1][j - 1] + 1
+        #             continue
                 
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        #         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
         
-        return dp[n][n]
+        # return dp[n][n]
