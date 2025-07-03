eb09349570cdefe9d0920716d1b519b0d6049051
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-03 11:49:17 -0700

    Time: 289 ms (80.45%), Space: 18.1 MB (87.09%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,36 +1,18 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
-        for i in range(len(s) + 1):
-            dp[i][0] = 1
-        
+        prev_dp = [0 for _ in range(len(t) + 1)]
+        prev_dp[0] = 1
+    
+
         for i in range(1, len(s) + 1):
+            curr_dp = [1]
             for j in range(1, len(t) + 1):
                 if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                    curr_dp.append(prev_dp[j - 1] + prev_dp[j])
                 else:
-                    dp[i][j] = dp[i - 1][j]
+                    curr_dp.append(prev_dp[j])
+            
+            prev_dp = curr_dp.copy()
         
-        return dp[len(s)][len(t)]
-
-
-        # def recursion(i, j):
-            
-        #     if j < 0:
-        #         return 1
-
-        #     if i < 0:
-        #         return 0
-            
-        #     if dp[i][j] != -1:
-        #         return dp[i][j]
-
-        #     if s[i] == t[j]:
-        #         dp[i][j] = recursion(i - 1, j - 1) + recursion(i - 1, j)
-        #     else:
-        #         dp[i][j] = recursion(i - 1, j)
-            
-        #     return dp[i][j]
-        
-        # return recursion(len(s) - 1, len(t) - 1)
+        return prev_dp[len(t)]
