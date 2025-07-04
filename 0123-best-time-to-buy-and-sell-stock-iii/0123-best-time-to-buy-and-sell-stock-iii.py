b4348d21501f63578675b7c5cbda7cc91fe30e2e
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-04 16:59:53 -0700

    Time: 1412 ms (16.66%), Space: 67.5 MB (16.46%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -3,27 +3,45 @@ class Solution:
 
         n = len(prices)
         
-        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
-        def recursion(index, buy, cap):
-            if cap == 0:
-                return 0
-            if index == n:
-                return 0
-            
-            if dp[index][buy][cap] != -1:
-                return dp[index][buy][cap]
-            
-            if buy:
-                dp[index][buy][cap] = max(
-                    -prices[index] + recursion(index + 1, 0, cap),
-                    0 + recursion(index + 1, 1, cap),
-                )
-            else:
-                dp[index][buy][cap] = max(
-                    prices[index] + recursion(index + 1, 1, cap - 1),
-                    0 + recursion(index + 1, 0, cap),
-                )
-            
-            return dp[index][buy][cap]
+        dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n + 1)]
+
+        for index in range(n - 1, -1, -1):
+            for buy in range(2):
+                for cap in range(1, 3):
+                    if buy:
+                        dp[index][buy][cap] = max(
+                            -prices[index] + dp[index + 1][0][cap],
+                            0 + dp[index + 1][1][cap],
+                        )
+                    else:
+                        dp[index][buy][cap] = max(
+                            prices[index] + dp[index + 1][1][cap - 1],
+                            0 + dp[index + 1][0][cap],
+                        )
         
-        return recursion(0, 1, 2)
+        return dp[0][1][2]
+
+
+        # def recursion(index, buy, cap):
+        #     if cap == 0:
+        #         return 0
+        #     if index == n:
+        #         return 0
+            
+        #     if dp[index][buy][cap] != -1:
+        #         return dp[index][buy][cap]
+            
+        #     if buy:
+        #         dp[index][buy][cap] = max(
+        #             -prices[index] + recursion(index + 1, 0, cap),
+        #             0 + recursion(index + 1, 1, cap),
+        #         )
+        #     else:
+        #         dp[index][buy][cap] = max(
+        #             prices[index] + recursion(index + 1, 1, cap - 1),
+        #             0 + recursion(index + 1, 0, cap),
+        #         )
+            
+        #     return dp[index][buy][cap]
+        
+        # return recursion(0, 1, 2)
