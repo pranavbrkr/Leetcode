71bc4794191505449df1af49b9446839a123d02a
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-03 21:26:20 -0700

    Time: 473 ms (18.27%), Space: 26.4 MB (35.71%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,31 +1,37 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:        
         
-        dp = [[-1 for _ in range(len(s))] for _ in range(len(p))]
-        def recursion(i, j):
-            if i < 0 and j < 0:
-                return True
-            
-            if i < 0 and j >= 0:
-                return False
-            
-            if j < 0 and i >= 0:
-                while i >= 0:
-                    if p[i] != "*":
-                        return False
-                    i -= 1                
-                return True
+        dp = [[False for _ in range(len(s) + 1)] for _ in range(len(p) + 1)]
 
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            if p[i] == "*":
-                dp[i][j] = recursion(i - 1, j) or recursion(i, j - 1)
-            elif p[i] == "?" or p[i] == s[j]:
-                dp[i][j] = recursion(i - 1, j - 1)
-            else:
-                dp[i][j] = False
-            
-            return dp[i][j]
+        dp[0][0] = True
+        for j in range(1, len(s) + 1):
+            dp[0][j] = False
         
-        return recursion(len(p) - 1, len(s) - 1)
+        for i in range(1, len(p) + 1):
+            dp[i][0] = True
+            for x in range(1, i + 1):
+                if p[x - 1] != "*":
+                    dp[i][0] = False
+                    break
+        
+        for i in range(1, len(p) + 1):            
+            for j in range(1, len(s) + 1):
+                if p[i - 1] == '*':
+                    dp[i][j]  = dp[i - 1][j] or dp[i][j - 1]
+                elif p[i - 1] == "?" or p[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = False
+        
+        return dp[len(p)][len(s)]
+            
+        #     if p[i] == "*":
+        #         dp[i][j] = recursion(i - 1, j) or recursion(i, j - 1)
+        #     elif p[i] == "?" or p[i] == s[j]:
+        #         dp[i][j] = recursion(i - 1, j - 1)
+        #     else:
+        #         dp[i][j] = False
+            
+        #     return dp[i][j]
+        
+        # return recursion(len(p) - 1, len(s) - 1)
