2a7a0f504ec7012bd29d655a07227a4c692a1d47
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-05 09:59:27 -0700

    Time: 99 ms (39.34%), Space: 25.8 MB (33.18%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -19,28 +19,3 @@ class Solution:
                         )
         
         return dp[0][1][k]
-
-        # def recursion(index, buy, rem):
-        #     if index == len(prices):
-        #         return 0
-            
-        #     if rem == 0:
-        #         return 0
-            
-        #     if dp[index][buy][rem] != -1:
-        #         return dp[index][buy][rem]
-            
-        #     if buy:
-        #         dp[index][buy][rem] = max(
-        #             -prices[index] + recursion(index + 1, 0, rem),
-        #             0 + recursion(index + 1, 1, rem)
-        #         )
-        #     else:
-        #         dp[index][buy][rem] = max(
-        #             prices[index] + recursion(index + 1, 1, rem - 1),
-        #             0 + recursion(index + 1, 0, rem)
-        #         )
-            
-        #     return dp[index][buy][rem]
-        
-        # return recursion(0, 1, k)
