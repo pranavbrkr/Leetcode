036f5a46a8fbea160208c4c6a262c4b85ba72903
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-05 11:57:18 -0700

    Time: 1098 ms (65.25%), Space: 18.2 MB (37.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,6 +3,7 @@ class Solution:
         n = len(nums)
         dp = [1] * n
         maxi = 1
+        last_index = 0
         backtrack = [i for i in range(n)]
 
         for index in range(n):
@@ -10,7 +11,16 @@ class Solution:
                 if nums[prev] < nums[index] and (1 + dp[prev]) > dp[index]:
                     dp[index] = 1 + dp[prev]
                     backtrack[index] = prev
-            maxi = max(maxi, dp[index])
+            if dp[index] > maxi:
+                maxi = dp[index]
+                last_index = index
 
+
+        lis = [nums[last_index]]
+
+        while backtrack[last_index] != last_index:
+            last_index = backtrack[last_index]
+            lis.insert(0, nums[last_index])
         
+        print(lis)
         return maxi
