71946fbbf586db792c988447a3c763579305beaf
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-07 11:31:49 -0700

    Time: 8 ms (99.02%), Space: 19.9 MB (65.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -8,9 +8,7 @@ class Solution:
 
         l, r = 0, 0
 
-        while l < s_len:
-            if r == g_len:
-                return g_len
+        while l < s_len and r < g_len:
             if g[r] <= s[l]:
                 r += 1
             l += 1
