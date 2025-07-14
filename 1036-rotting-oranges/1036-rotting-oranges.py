bc74cc50f6e1a9d8da16c847a49a013083d6fe84
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-14 14:29:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,29 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         time = 0
-        fresh = 0
         m = len(grid)
         n = len(grid[0])
-        q = deque()
+
+        rotten = deque([])
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    fresh += 1
                 if grid[i][j] == 2:
-                    q.append([i, j])
-        
-        while q and fresh > 0:
-            for i in range(len(q)):
-                r, c = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-                for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if row < 0 or row >= m or col < 0 or col >= n or grid[row][col] != 1:
-                        continue
-                    grid[row][col] = 2
-                    q.append([row, col])
-                    fresh -= 1
-            time += 1
+                    rotten.append((i, j))
 
-        return time if fresh == 0 else -1
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
+        while rotten:
+            for i in range(len(rotten)):
+                o_row, o_col = rotten.popleft()
+                for dr, dc in directions:
+                    nr = dr + o_row
+                    nc = dc + o_col
+                    if nr in range(m) and nc in range(n) and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        rotten.append((nr, nc))
+            time += 1
+        
+
+        print(grid)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    return -1
+        
+        return time
