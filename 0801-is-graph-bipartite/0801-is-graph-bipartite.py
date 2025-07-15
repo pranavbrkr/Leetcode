d4bb45aa630d33e4eb42a7a012e1bdb393385def
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-14 18:11:34 -0700

    Time: 3 ms (43.86%), Space: 18.8 MB (9.32%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -3,19 +3,21 @@ class Solution:
         n = len(graph)
         color = [-1] * n
 
-        for start in range(n):
-            if color[start] == -1:
-                queue = deque()
-                queue.append(start)
-                color[start] = 0
-                while queue:
-                    node = queue.popleft()
-                    for adj in graph[node]:
-                        if color[adj] == -1:
-                            color[adj] = not color[node]
-                            queue.append(adj)
-                            print(queue)
-                        elif color[adj] == color[node]:
-                            return False
-                
+        def dfs(node, col):
+            color[node] = col
+
+            for adj in graph[node]:
+                if color[adj] == -1:
+                    if not dfs(adj, not col):
+                        return False
+                elif color[adj] == col:
+                    return False
+            
+            return True
+
+        for node in range(n):
+            if color[node] == -1:
+                if not dfs(node, 0):
+                    return False
+        
         return True
