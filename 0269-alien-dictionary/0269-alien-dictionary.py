81f1ec5858fa8675143982dbeb07c2c57c622462
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-07-17 12:02:10 -0700

    Time: 2 ms (46.89%), Space: 17.9 MB (34.62%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,35 +1,36 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = {c: set() for w in words for c in w}
-        indegree = {c: 0 for c in adj}
+        graph = {c: set() for w in words for c in w}
+        indegree = {c: 0 for c in graph}
 
         for i in range(len(words) - 1):
             w1 = words[i]
             w2 = words[i + 1]
 
             min_len = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
+            if w1[:min_len] == w2[:min_len] and len(w1) > len(w2):
                 return ""
+            
             for j in range(min_len):
                 if w1[j] != w2[j]:
-                    if w2[j] not in adj[w1[j]]:
-                        adj[w1[j]].add(w2[j])
+                    if w2[j] not in graph[w1[j]]:
+                        graph[w1[j]].add(w2[j])
                         indegree[w2[j]] += 1
                     break
         
-        q = deque([c for c in indegree if indegree[c] == 0])
+        queue = deque([c for c in indegree if indegree[c] == 0])
         answer = []
-        print(indegree)
 
-        while q:
-            c = q.popleft()
-            answer.append(c)
-            for nei in adj[c]:
+        while queue:
+            node = queue.popleft()
+            answer.append(node)
+
+            for nei in graph[node]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
-                    q.append(nei)
+                    queue.append(nei)
         
         if len(answer) != len(indegree):
             return ""
-        
+
         return "".join(answer)
