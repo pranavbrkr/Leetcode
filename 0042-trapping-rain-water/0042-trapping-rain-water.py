728ebcf6b85c9ed1d69c059f60795e5d0ef10ab2
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-07 13:58:57 -0700

    Time: 34 ms (13.57%), Space: 19.8 MB (5.25%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,37 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        max_left = []
+        max_right = []
         n = len(height)
 
-        if n == 0 or n == 1:
-            return 0
+        max_height = 0
+        for i in range(n):
+            max_left.append(max_height)
+            max_height = max(max_height, height[i])
 
-        left, right = 0, n - 1
-        max_left, max_right = height[left], height[right]
+        max_height = 0
+        for i in range(n - 1, -1, -1):
+            max_right.append(max_height)
+            max_height = max(max_height, height[i])
+        max_right.reverse()
 
-        left += 1
-        right -= 1
+        print(max_left)
+        print(max_right)
+
 
         water = 0
-
-        while left <= right:
-            if max_left <= max_right:
-                if max_left - height[left] > 0:
-                    water += max_left - height[left]
-                    print(f"Water {water} at left {left}")
-            else:
-                if max_right - height[right] > 0:
-                    water += max_right - height[right]
-                    print(f"Water {water} at right {right}")
-
-            if max_left <= max_right:
-                if max_left < height[left]:
-                    max_left = height[left]
-                left += 1
-            else:
-                if max_right < height[right]:
-                    max_right = height[right]
-                right -= 1
-            
-            print(f"l: {left}, r: {right}")
-
+        for i in range(n):
+            if (min(max_left[i], max_right[i]) - height[i]) > 0:
+                water += min(max_left[i], max_right[i]) - height[i]
+        
         return water
