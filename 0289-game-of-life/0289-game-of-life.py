51be8bbfb91f879a77328bb8fa39249b01f9827d
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-07 13:44:35 -0700

    Time: 0 ms (100%), Space: 17.8 MB (47.93%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -3,36 +3,27 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # Original | New | state
-        #     0    |  0  |   0
-        #     1    |  0  |   1
-        #     0    |  1  |   2
-        #     1    |  1  |   3
-
         rows, cols = len(board), len(board[0])
 
-        def countNeighbours(r, c):
-            num_neighbours = 0
+        def countNeighbors(r, c):
+            num_neighbors = 0
             for i in range(r - 1, r + 2):
                 for j in range(c - 1, c + 2):
-                    if ((i == r and j == c) or
-                        i < 0  or j < 0 or i == rows or j == cols):
+                    if (i == r and j == c) or i < 0 or j < 0 or i == rows or j == cols:
                         continue
                     if board[i][j] in [1, 3]:
-                        num_neighbours += 1
-
-            return num_neighbours
-
+                        num_neighbors += 1
+            return num_neighbors
+        
         for i in range(rows):
             for j in range(cols):
-                num_neighbours = countNeighbours(i, j)
-
+                neighbors = countNeighbors(i, j)
                 if board[i][j]:
-                    if num_neighbours in [2, 3]:
+                    if neighbors in [2, 3]:
                         board[i][j] = 3
-                elif num_neighbours == 3:
+                elif neighbors == 3:
                     board[i][j] = 2
-
+        
         for i in range(rows):
             for j in range(cols):
                 if board[i][j] == 1:
