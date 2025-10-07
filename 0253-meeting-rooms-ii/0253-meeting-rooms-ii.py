13b18cf0b0a9ca5fdd4f857b07359be9a2f1910e
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-06 21:54:41 -0700

    Time: 6 ms (38.73%), Space: 20.1 MB (36.9%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -2,9 +2,10 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         starts = sorted([i[0] for i in intervals])
         ends = sorted([i[1] for i in intervals])
+
+        s, e = 0, 0
         answer = 0
         count = 0
-        s, e = 0, 0
 
         while s < len(intervals):
             if starts[s] < ends[e]:
