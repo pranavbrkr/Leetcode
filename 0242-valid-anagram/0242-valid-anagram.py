469079d289cc713a6a75b7f2e6b9f04814b4098d
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-06 18:26:16 -0700

    Time: 12 ms (52.24%), Space: 17.8 MB (64.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,12 +5,12 @@ class Solution:
         
         counter = [0] * 26
 
-        for i in range(len(s)):
-            counter[ord(s[i]) - ord('a')] += 1
+        for i, ch in enumerate(s):
+            counter[ord(ch) - ord('a')] += 1
             counter[ord(t[i]) - ord('a')] -= 1
         
-        for alpha_count in counter:
-            if alpha_count != 0:
+        for cnt in counter:
+            if cnt != 0:
                 return False
-
+        
         return True
