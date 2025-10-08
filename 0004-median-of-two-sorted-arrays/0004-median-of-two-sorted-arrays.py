e33a0c9cab8fca6ba875f793afe65decacc54c84
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-07 18:39:16 -0700

    Time: 0 ms (100%), Space: 18.2 MB (19.42%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -4,22 +4,24 @@ class Solution:
         total = len(nums1) + len(nums2)
         half = total // 2
 
-        if len(A) > len(B):
+        if len(B) < len(A):
             A, B = B, A
-
+        
         l, r = 0, len(A) - 1
         while True:
-            i = (l + r) // 2
-            j = half - i - 2
+            i = (l + r) // 2  # middle of A
+            j = half - i - 2  #  middle of B
 
-            Aleft = A[i] if i >= 0  else float('-inf')
+            Aleft = A[i] if i >= 0 else float('-inf')
             Aright = A[i + 1] if (i + 1) < len(A) else float('inf')
-            Bleft = B[j] if j >= 0  else float('-inf')
+            Bleft = B[j] if j >= 0 else float('-inf')
             Bright = B[j + 1] if (j + 1) < len(B) else float('inf')
 
             if Aleft <= Bright and Bleft <= Aright:
+                # odd
                 if total % 2:
                     return min(Aright, Bright)
+                # even
                 else:
                     return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
             elif Aleft > Bright:
