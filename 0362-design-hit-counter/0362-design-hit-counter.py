6cd2c01c3636be0c77a175b56774b5b005bae5e7
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-08 12:17:00 -0700

    Time: 0 ms (100%), Space: 18 MB (20.32%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -1,22 +1,28 @@
 class HitCounter:
 
     def __init__(self):
-        self.hits = deque()
+        self.hits = []
         self.total_hits = 0
 
     def hit(self, timestamp: int) -> None:
-        if self.hits and self.hits[-1][0] == timestamp:
-            self.hits[-1] = (timestamp, self.hits[-1][1] + 1)
-        else:
-            self.hits.append((timestamp, 1))
+        self.hits.append(timestamp)
         self.total_hits += 1
+        
 
     def getHits(self, timestamp: int) -> int:
-        while self.hits and (timestamp - self.hits[0][0]) >= 300:
-            self.total_hits -= self.hits[0][1]
-            self.hits.popleft()
-        
-        return self.total_hits
+        target = timestamp - 300
+
+        left, right = 0, self.total_hits - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if self.hits[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return self.total_hits - left
 
 
 # Your HitCounter object will be instantiated and called as such:
