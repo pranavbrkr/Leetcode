7b05eba9376b6cd30c8c742751399f3fa6ff5e06
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-08 13:46:19 -0700

    Time: 13 ms (87.15%), Space: 19.8 MB (6.13%) - LeetHub

--- 1905-design-authentication-manager/1905-design-authentication-manager.py
+++ 1905-design-authentication-manager/1905-design-authentication-manager.py
@@ -1,24 +1,51 @@
+class Node:
+    def __init__(self, tokenId, ttl = -1):
+        self.token_id, self.ttl = tokenId, ttl
+        self.next, self.prev = None, None
+
 class AuthenticationManager:
 
     def __init__(self, timeToLive: int):
-        self.ttl = timeToLive
-        self.tokens = {}
+        self.cache = {}
+        self.time_to_live = timeToLive
+        self.size = 0
+        self.right, self.left = Node(-1), Node(-1)
+        self.right.prev, self.left.next = self.left, self.right
+    
+    def insert(self, node):
+        prv = self.right.prev
+        prv.next = node
+        node.prev = prv
+        node.next = self.right
+        self.right.prev = node
+
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
 
     def generate(self, tokenId: str, currentTime: int) -> None:
-        self.tokens[tokenId] = currentTime + self.ttl
+        if tokenId in self.cache:
+            self.remove(self.cache[tokenId])
+            self.size -= 1
+        self.cache[tokenId] = Node(tokenId, currentTime + self.time_to_live)
+        self.insert(self.cache[tokenId])
+        self.size += 1
 
     def renew(self, tokenId: str, currentTime: int) -> None:
-        if tokenId in self.tokens and currentTime < self.tokens[tokenId]:
-            self.tokens[tokenId] = currentTime + self.ttl
+        node = self.cache.get(tokenId, None)
+        if node and node.ttl > currentTime:
+            self.remove(node)
+            self.cache[tokenId] = Node(tokenId, currentTime + self.time_to_live)
+            self.insert(self.cache[tokenId])
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        unexpired_tokens = 0
-        for token_ttl in self.tokens.values():
-            if currentTime < token_ttl:
-                unexpired_tokens += 1
+        while self.left.next is not self.right and self.left.next.ttl <= currentTime:
+            expired_node = self.left.next
+            self.remove(expired_node)
+            del self.cache[expired_node.token_id]
+            self.size -= 1
         
-        return unexpired_tokens
-
+        return self.size
 
 # Your AuthenticationManager object will be instantiated and called as such:
 # obj = AuthenticationManager(timeToLive)
