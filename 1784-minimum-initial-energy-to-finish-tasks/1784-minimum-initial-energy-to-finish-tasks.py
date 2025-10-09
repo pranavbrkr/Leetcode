4d01bb0a8fdb833cfb1cd23c9f469c0ba6b07bad
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-08 18:42:38 -0700

    Time: 52 ms (81.03%), Space: 53.9 MB (15.52%) - LeetHub

--- 1784-minimum-initial-energy-to-finish-tasks/1784-minimum-initial-energy-to-finish-tasks.py
+++ 1784-minimum-initial-energy-to-finish-tasks/1784-minimum-initial-energy-to-finish-tasks.py
@@ -3,12 +3,12 @@ class Solution:
         tasks.sort(key = lambda x: x[0] - x[1])
 
         answer = 0
-        curr = 0
+        curr_energy = 0
 
-        for cost, mini in tasks:
-            if mini > curr:
-                answer += (mini - curr)
-                curr = mini
-            curr -= cost
+        for cost, req in tasks:
+            if req > curr_energy:
+                answer += (req - curr_energy)
+                curr_energy = req
+            curr_energy -= cost
         
         return answer
