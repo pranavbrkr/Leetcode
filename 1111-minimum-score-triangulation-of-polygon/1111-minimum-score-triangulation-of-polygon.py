0e8f55cab6a97d0c83855366e334652ce3d2cf91
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-08 19:22:44 -0700

    Time: 35 ms (86.3%), Space: 18.1 MB (53.66%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
@@ -1,22 +1,15 @@
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         n = len(values)
-        dp = [[-1] * n for _ in range(n)]
+        dp = [[0] * n for _ in range(n)]
 
-        def dfs(i, j):
-            # no triangle if fewer than 2 vertices
-            if j - i < 2:
-                return 0
-
-            if dp[i][j] != -1:
-                return dp[i][j]
-
-            best = float('inf')
-            for k in range(i + 1, j):
-                cost = values[i] * values[k] * values[j] + dfs(i, k) + dfs(k, j)
-                best = min(best, cost)
-
-            dp[i][j] = best
-            return best
+        for L in range(2, n):
+            for i in range(0, n - L):
+                j = i + L
+                best = float('inf')
+                for k in range(i + 1, j):
+                    cost = dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]
+                    best = min(cost, best)
+                dp[i][j] = best
         
-        return dfs(0, n - 1)
+        return dp[0][n - 1]
