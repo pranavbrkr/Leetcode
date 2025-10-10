5dcf7135951b7934858af9bdd3df0e99393ff11b
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-09 20:10:11 -0700

    Time: 19 ms (64.95%), Space: 18 MB (19.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,49 +1,36 @@
-
-from collections import defaultdict
-
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        m = len(s1)
-        n = len(s2)
-        s1_count = defaultdict(int)
-        s2_count = defaultdict(int)
+        if len(s1) > len(s2):
+            return False
+        
+        s1_count, s2_count = [0] * 26, [0] * 26
 
-        for s in s1:
-            s1_count[s] += 1
-
-        for s in s2[:m]:
-            s2_count[s] += 1
-
-        start = 0
-        end = m - 1
-
+        for i  in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
         matches = 0
-        alphabets = "abcdefghijklmnopqrstuvwxyz"
+        for i in range(26):
+            matches += 1 if s1_count[i] == s2_count[i] else 0
         
-        for alphabet in alphabets:
-            if s1_count[alphabet] == s2_count[alphabet]:
-                matches += 1
-
-        if matches == 26:
-            return True
-        
-        s2_count[s2[start]] -= 1
-        start +=1
-        end += 1
-
-
-        while end < n:
-            s2_count[s2[end]] += 1
-            matches = 0
-            for alphabet in alphabets:
-                if s1_count[alphabet] == s2_count[alphabet]:
-                    matches += 1
-            
+        l = 0
+        for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
             
-            s2_count[s2[start]] -= 1
-            start +=1
-            end += 1
-
-        return False
+            index = ord(s2[r]) - ord('a')
+            s2_count[index] += 1
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] + 1 == s2_count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2_count[index] -= 1
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] - 1 == s2_count[index]:
+                matches -= 1
+            l += 1
+        
+        return matches == 26
