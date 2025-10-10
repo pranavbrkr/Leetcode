c3ee3b3cfdcf8a354525e49d14eb68bb7f610fef
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-09 18:59:09 -0700

    Time: 7 ms (36.22%), Space: 17.8 MB (44.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,33 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        for i in range(9):
-            h_set = set()
-            v_set = set()
+        squares = {}
 
-            for j in range(9):
-                if board[i][j] in h_set:
-                    return False
-                if board[i][j] != '.':
-                    h_set.add(board[i][j])
-                
-                if board[j][i] in v_set:
-                    return False
-                if board[j][i] != '.':
-                    v_set.add(board[j][i])
-        
-        box_dict = {}
         for i in range(9):
+            cols = set()
+            rows = set()
             a = i // 3
-            for j in range(9):
+            for j  in range(9):
                 b = j // 3
-
-                if (a, b) not in box_dict:
-                    box_dict[(a, b)] = set()
-                
-                if board[i][j] in box_dict[(a, b)]:
+                # check row
+                if board[i][j] in rows:
                     return False
-                if board[i][j] != '.':
-                    box_dict[(a, b)].add(board[i][j])
-
+                elif board[i][j] != '.':
+                    rows.add(board[i][j])
+                
+                # check column
+                if board[j][i] in cols:
+                    return False
+                elif board[j][i] != '.':
+                    cols.add(board[j][i])
+                
+                # check squares
+                if (a, b) not in squares.keys():
+                    squares[(a, b)] = set()
+                
+                if board[i][j] in squares[(a, b)]:
+                    return False
+                elif board[i][j] != '.':
+                    squares[(a, b)].add(board[i][j])
+        
         return True
