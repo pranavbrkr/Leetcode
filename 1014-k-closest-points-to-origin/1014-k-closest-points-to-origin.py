290108fa1086a68c22fc02d125a74feff75025b2
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-11 19:42:38 -0700

    Time: 90 ms (24.92%), Space: 22.7 MB (51.15%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,16 +1,16 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-
-        def euclidean(x1, y1):
-            return (x1 * x1 + y1 * y1) ** (0.5)
-
         
+        def euclidean(x1, y1):
+            return (x1 * x1 + y1 * y1) ** 0.5
+        
+
         heap = []
         heapq.heapify(heap)
 
         for point in points:
             heapq.heappush(heap, (euclidean(point[0], point[1]), point))
-
+        
         answer = []
 
         for i in range(k):
