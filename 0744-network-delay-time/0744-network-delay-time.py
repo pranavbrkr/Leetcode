f4d7affcff775739c99afd61e5e8dea6fd798a93
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-13 21:51:01 -0700

    Time: 374 ms (73.94%), Space: 19.7 MB (76.24%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -4,21 +4,22 @@ class Solution:
 
         for u, v, w in times:
             edges[u].append((v, w))
-
-        min_heap = [(0, k)]
+        
+        heap = [(0, k)]
         visited = set()
 
-        t = 0
-        
-        while min_heap:
-            w1, n1 = heapq.heappop(min_heap)
+        time = 0
+
+        while heap:
+            w1, n1 = heapq.heappop(heap)
             if n1 in visited:
                 continue
+            
             visited.add(n1)
-            t = max(t, w1)
-
+            time = max(time, w1)
+            
             for n2, w2 in edges[n1]:
                 if n2 not in visited:
-                    heapq.heappush(min_heap, (w1 + w2, n2))
+                    heapq.heappush(heap, (w1 + w2, n2))
         
-        return t if len(visited) == n else -1
+        return time if len(visited) == n else -1
