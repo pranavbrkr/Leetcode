3fc9668eee3f4d9b342fad524752522046e760c6
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-13 19:09:56 -0700

    Time: 6 ms (28.44%), Space: 18 MB (25.83%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        time = 0
-        m = len(grid)
-        n = len(grid[0])
-        fresh = 0
-
-        rotten = deque([])
+        m, n = len(grid), len(grid[0])
+        time, fresh = 0, 0
+        rotten = deque()
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 2:
-                    rotten.append((i, j))
                 if grid[i][j] == 1:
                     fresh += 1
-
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                elif grid[i][j] == 2:
+                    rotten.append((i, j))
         
         while rotten and fresh:
-            for i in range(len(rotten)):
+            rotten_len = len(rotten)
+            for _ in range(rotten_len):
                 r, c = rotten.popleft()
                 for dr, dc in directions:
-                    nr = dr + r
-                    nc = dc + c
+                    nr, nc = r + dr, c + dc
                     if nr not in range(m) or nc not in range(n) or grid[nr][nc] != 1:
                         continue
+                    rotten.append((nr, nc))
                     grid[nr][nc] = 2
                     fresh -= 1
-                    rotten.append((nr, nc))
             time += 1
         
         return -1 if fresh else time
