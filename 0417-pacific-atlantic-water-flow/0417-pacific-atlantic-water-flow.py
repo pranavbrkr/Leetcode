3da91e109c4ad2f1e140faad5b86193f31de6b3a
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-13 19:21:10 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,27 +4,25 @@ class Solution:
         pac, atl = set(), set()
 
         def dfs(r, c, visited, prev_height):
-            if (r, c) in visited or r < 0 or r >= m or c < 0 or c >= n or prev_height > heights[r][c]:
+            if (r, c) in visited or r not in range(m) or c not in range(n) or prev_height > heights[r][c]:
                 return
-            
             visited.add((r, c))
             dfs(r + 1, c, visited, heights[r][c])
             dfs(r - 1, c, visited, heights[r][c])
             dfs(r, c + 1, visited, heights[r][c])
             dfs(r, c - 1, visited, heights[r][c])
-
         
         for i in range(m):
             dfs(i, 0, pac, heights[i][0])
             dfs(i, n - 1, atl, heights[i][n - 1])
-
+        
         for i in range(n):
             dfs(0, i, pac, heights[0][i])
-            dfs(m - 1, i, atl, heights[m - 1][i])
-
+            dfs(n - 1, i, atl, heights[m - 1][i])
+        
         answer = []
         for (r, c) in pac:
             if (r, c) in atl:
-                answer.append((r, c))
-
+                answer.append([r, c])
+        
         return answer
