d9032cbc4ce256dee4d674cffd95e91f025b757f
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-13 20:58:39 -0700

    Time: 3 ms (47.53%), Space: 20.1 MB (7.55%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -3,22 +3,23 @@ class Solution:
         if not n:
             return True
         
-        visited = set()
         adj = {i: [] for i in range(n)}
+
         for n1, n2 in edges:
             adj[n1].append(n2)
             adj[n2].append(n1)
         
-        def dfs(i, prev):
-            if i in visited:
+        visited = set()
+        def dfs(node, prev):
+            if node in visited:
                 return False
             
-            visited.add(i)
-            for j in adj[i]:
-                if j == prev:
+            visited.add(node)
+            for nei in adj[node]:
+                if nei == prev:
                     continue
-                if not dfs(j, i):
+                if not dfs(nei, node):
                     return False
             return True
         
-        return dfs(0, -1) and n == len(visited)
+        return dfs(0, -1) and len(visited) == n
