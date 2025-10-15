1a5cf5b8d515626c926a516d55cbfbf560b359cd
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-15 00:56:53 -0700

    Time: 3 ms (93.95%), Space: 18 MB (27.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,16 +1,25 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
+        cache = {}
+
         def dfs(i, j):
             if j == n:
                 return i == m
+
+            if (i, j) in cache:
+                return cache[(i, j)]
             
             match = i < m and (s[i] == p[j] or p[j] == '.')
             if (j + 1) < n  and p[j + 1] == '*':
-                return (dfs(i, j + 2) or (match and dfs(i + 1, j)))
-            
+                cache[(i, j)] = (dfs(i, j + 2) or (match and dfs(i + 1, j)))
+                return cache[(i, j)]
+
             if match:
-                return dfs(i + 1, j + 1)
-            return False
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+            
+            cache[(i, j)] = False
+            return cache[(i, j)]
         
         return dfs(0, 0)
