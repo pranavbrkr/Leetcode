3e2d14c5e84f694fc4709cbf8ddbaf7c4fe8b275
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-15 00:32:24 -0700

    Time: 2802 ms (70.88%), Space: 20.6 MB (56.99%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -3,19 +3,16 @@ class Solution:
         n = len(nums)
         nums.append(1)
         nums.insert(0, 1)
-        dp = {}
 
-        def partition(i, j):
-            if i > j:
-                return 0
-            if (i, j) in dp:
-                return dp[(i, j)]
-            max_coins = float('-inf')
-            for index in range(i, j + 1):
-                cost = nums[i - 1] * nums[index] * nums[j + 1] + partition(i, index - 1) + partition(index + 1, j)
-                max_coins = max(max_coins, cost)
-            
-            dp[(i, j)] = max_coins
-            return dp[(i, j)]
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
+        for i in range(n, 0, -1):
+            for j in range(1, n + 1):
+                if i > j:
+                    continue
+                max_coins = float('-inf')
+                for index in range(i, j + 1):
+                    cost = nums[i - 1] * nums[index] * nums[j + 1] + dp[i][index - 1] + dp[index + 1][j]
+                    max_coins = max(max_coins, cost)
+                dp[i][j] = max_coins
         
-        return partition(1, n)
+        return dp[1][n]
