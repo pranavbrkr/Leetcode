8e49ca59ee06dd7f9350c99ba5b8a655f98b76d6
Pranav Borikar <31160043+pranavbrkr@users.noreply.github.com>
2025-10-14 23:41:46 -0700

    Time: 423 ms (56.64%), Space: 74.3 MB (38.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,19 +1,15 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        dp = {}
-        def backtrack(i, j):
-            if j < 0:
-                return 1
-            if i < 0:
-                return 0
-            if (i, j) in dp:
-                return dp[(i, j)]
-            
-            if s[i] == t[j]:
-                dp[(i, j)] = backtrack(i - 1, j - 1) + backtrack(i - 1, j)
-            else:
-                dp[(i, j)] = backtrack(i - 1, j)
-            
-            return dp[(i, j)]
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+
+        for i in range(len(s) + 1):
+            dp[i][0] = 1
         
-        return backtrack(len(s) - 1, len(t) - 1)
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(t) + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[len(s)][len(t)]
